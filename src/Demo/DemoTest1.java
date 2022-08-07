83527b407d1ada67d9ce5d1c80b12cc1a401d9d0
zdoue <zdoue@10.0.0.198>
2022-08-06 23:12:56 -0400

    coomit

--- src/Demo/DemoTest1.java
+++ src/Demo/DemoTest1.java
@@ -10,5 +10,8 @@ public class DemoTest1 {
    public static void add(int a,int b) {
 	   System.out.println(a+b);
    }
-   
+  
+   public static void substract(int a,int b) {
+	   System.out.println(a/b);
+}
 }

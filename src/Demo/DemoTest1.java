41451bbfda1bbb552b9c89042b81d08d82ae3b43
Houeslati1 <110270650+Houeslati1@users.noreply.github.com>
2022-08-06 23:24:49 -0400

    Update DemoTest1.java
    my name

--- src/Demo/DemoTest1.java
+++ src/Demo/DemoTest1.java
@@ -13,5 +13,8 @@ public class DemoTest1 {
   
    public static void substract(int a,int b) {
 	   System.out.println(a/b);
-}
+}    //123                                                                                                                                       
+	public static void main(String[] args) {
+	
+		System.out.println("Hedia...Oueslati");
 }
